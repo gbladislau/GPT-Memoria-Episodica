96f95b6604754e579c19b3f0f2c31ce5dc88ae94
guilherme751 <guisgb10@gmail.com>
2025-03-20 21:46:20 -0300

    data loader

--- fact_extraction.py
+++ fact_extraction.py
@@ -1,50 +1,36 @@
-import random
 import json
 
 # Function to extract facts
-def extract_facts(dataset, num_examples = 10):
+def extract_facts(dataset, num_facts = 10):
     facts = []
+    questions = dataset.get("question")[:num_facts]
+    answers = dataset.get("answer")[:num_facts] 
+    contexts = dataset.get("search_results")[:num_facts]
     
-    answers = dataset.get("answer")[:num_examples] 
-    contexts = dataset.get("search_results")[:num_examples]
-    
-    for i,c in enumerate(contexts):
-        # correct_answers = answers[i].get("normalized_aliases")
-        search_contexts = c.get("search_context")
+   
+    for i in range(num_facts):
+        question = questions[i]
+        c_answers = answers[i].get("normalized_aliases")
+        search_contexts = contexts[i].get("search_context")
+        
         if (len(search_contexts) > 0):
-            facts.append(search_contexts[0])
-        else:
-            print("NO CONTEXT")
+            context = search_contexts[0]
+            facts.append((question, c_answers, context))
+        
+    
+    return facts
+        
 
+def getDataLoader(json_path, num_facts):
+    with open(json_path, "r") as f:
+        dataset = json.load(f)
+        
+    facts = extract_facts(dataset, num_facts)
+    
     return facts
 
 
-def loadJSON():
-    with open("trivia_qa.json", "r") as f:
-        dataset = json.load(f)
-        
-    facts = extract_facts(dataset, 100)
-    return facts, dataset.get("question")
-    
-    
-    
-    
-# def factsPrompt(llm, facts):
-#     intro_message = "I will start giving you some facts and later ask questions about them\n"
-    
-#     llm(intro_message, max_new_tokens=100)[0]["generated_text"]
-    
-#     for i, fact in enumerate(facts):
-#         message = f"FACT {i}\n" + fact + "END OF FACT {i}\n\n"
-#         llm(message, max_new_tokens=100)[0]["generated_text"]
+data = getDataLoader("trivia_qa.json", 100)
 
+print(len(data))
 
-facts, questions = loadJSON()
-for f in facts[:2]:
-    print("Starting fact:\n")
-    print(f)
-    print("Ending fact\n")
-
-print(questions[:2])
-
-# factsPrompt(facts)
